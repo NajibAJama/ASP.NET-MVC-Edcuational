532d3d7ab02746d8b5d08fb88d073178d043929e
NajibAJama <NajibAJama@gmail.com>
2018-10-01 22:13:16 -0700

    Added  GigFromViewModel and Gig input form

--- GigHub/GigHub/Controllers/GigsController.cs
+++ GigHub/GigHub/Controllers/GigsController.cs
@@ -1,17 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using GigHub.Models;
+using GigHub.ViewModels;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace GigHub.Controllers
 {
     public class GigsController : Controller
     {
+        private readonly ApplicationDbContext _context;
         // GET: Gigs
-        public ActionResult Create() 
+        //initialize _context
+        public GigsController()
         {
-            return View();
+            _context = new ApplicationDbContext();
+        }
+
+        //create view model and set the genera properties
+        public ActionResult Create()
+        {
+            var viewModel = new GigFormViewModel
+            {
+                Genres = _context.Genres.ToList()
+        };
+
+
+            return View(viewModel);
         }
     }
 }
---
+++ GigHub/GigHub/ViewModels/GigFormViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace GigHub.ViewModels
+{
+    public class GigFormViewModel
+    {
+        public string Venue { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+
+        //for drop down list we need numeric value for each option
+        //this int will be genre Id.
+        public int Genre { get; set; }
+        //list of genere from the database
+        //we wont add anything to this list or use it's options 
+        //using index.(list and collections are unecesory)
+        public IEnumerable<Genre> Genres  { get; set; }
+
+    }
+}
